450d81f23a7c2a169120fb1c50d37c5d621a759c
Naveen Kumar <61756691+Navin248@users.noreply.github.com>
2025-05-20 02:06:20 +0530

    Update decrypt.py
    removed keys

--- decrypt.py
+++ decrypt.py
@@ -1,12 +1,12 @@
 from cryptography.fernet import Fernet
 import re
 
-# Encryption key
-key = b'7lGUTamn1KkNneBvSkvLvo_1T5TZrXgh592pFiarJOc='
+# Encryption key(generate your own)
+key = b''
 cipher = Fernet(key)
 
-# Encrypted content
-encrypted = b"gAAAAABoEkDNIYZG7dSqhEe_ZfMSJlOsDwNObGkoo-fqxWXX2jBjaRZ8lsI1RpoAyUYfDMDgAakflvKmzaS-_zJeW8FojNDlbDzm5F8GXmRzVpCW3cWy9cwXQv9I-Ya0t8QhLNyGw_HyBBLvMVDbJ0nX1Q7_aQOBLpTHGOQmGxUk7RLkxDxaqraMd0ObnT2LieAhEVeZrexgSqUF697HHjHJGQUcq-23EtQEfRXSj96mx307jHrFY0C1SH3lXlnxVxbQnBU4vwVUBjZ5AQzPo8JSABdraER_3S731sgnfE484fi_BENucOY="
+# Encrypted content inside quotes
+encrypted = b""
 
 # Decrypt the content
 decrypted = cipher.decrypt(encrypted).decode()
